591aaf5a6c546c8a2ff39f1781fc1c589ac4a864
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-04-01 20:31:32 +0600

    dynamic route

---
+++ src/components/FriendDetail/FriendDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const FriendDetail = () => {
+  const param = useParams();
+
+  return (
+    <div>
+      <h2>This is detail {param.friendId}</h2>
+    </div>
+  );
+};
+
+export default FriendDetail;
